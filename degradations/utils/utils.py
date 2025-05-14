89434776827d305b6d758c12b367e2154f3334d2
Raphaël R <93404597+Raphael-Renard@users.noreply.github.com>
2025-05-14 10:31:19 +0200

    fix: faded

--- degradations/utils/utils.py
+++ degradations/utils/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def load_image(input_path, to_grayscale=True):
     """
@@ -52,3 +53,42 @@ def resize_image(image, downscale_factor):
     new_size = (int(image.shape[1] * downscale_factor), int(image.shape[0] * downscale_factor))
     print(f"Image resized to: {new_size}")
     return cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+
+
+
+
+def remove_black_borders(image):
+    """Supprime les bords noirs qui ont été rajoutés pour le dataloader."""
+    # Créer un masque détectant les pixels non noirs
+    mask = np.any(image > 0, axis=2)
+    
+    # Trouver les coordonnées des pixels non noirs
+    coords = np.column_stack(np.where(mask))
+
+    if len(coords) == 0:
+        return image
+
+    # Trouver la boîte englobante des pixels non noirs
+    y_min, x_min = coords.min(axis=0)
+    y_max, x_max = coords.max(axis=0)
+
+    # Recadre image
+    cropped_image = image[y_min:y_max+1, x_min:x_max+1]
+
+    return cropped_image, (y_min, x_min, y_max, x_max)  # image recadrée et coordonnées du recadrage
+
+def restore_black_borders(original_shape, cropped_image, crop_coords):
+    """Restaure les bords noirs après traitement, en gardant la taille originale."""
+    h_original, w_original = original_shape[:2]
+    h_cropped, w_cropped = cropped_image.shape[:2]
+
+    # Créer une image noire avec la taille originale
+    restored_image = np.zeros((h_original, w_original, 3), dtype=np.uint8)
+
+    # Récupérer les anciennes coordonnées de la zone non noire
+    y_min, x_min, _, _ = crop_coords
+
+    # Coller l’image recadrée au bon emplacement
+    restored_image[y_min:y_min+h_cropped, x_min:x_min+w_cropped] = cropped_image
+
+    return restored_image
